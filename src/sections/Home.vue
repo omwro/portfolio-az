d9d62dc010049d6f5cb827390fb528b08ff88091
Omer Erdem <m.o.erdem@outlook.com>
2022-02-23 18:30:20 +0100

    home + navbar fixed

--- src/sections/Home.vue
+++ src/sections/Home.vue
@@ -2,6 +2,8 @@
     <section id="home" class="max-width">
         <Selfie/>
         <Typer/>
+        <div class="gradient-circle"></div>
+        <div class="gradient-circle two"></div>
     </section>
 </template>
 
@@ -31,5 +33,39 @@ export default {
     text-align: center;
     align-items: center;
     justify-content: center;
+    position: relative;
+    overflow: hidden;
+    margin-top: 0;
+    margin-bottom: 0;
+    padding-top: 96px;
+    padding-bottom: 32px;
+
+     .gradient-circle {
+        position: absolute;
+        left: -250px;
+        top: 250px;
+        right: auto;
+        bottom: auto;
+        z-index: -1;
+        width: 500px;
+        height: 500px;
+        border-radius: 70%;
+        background-image: -webkit-linear-gradient(45deg, #059dff, #36c6f0);
+        background-image: -o-linear-gradient(45deg, #059dff, #36c6f0);
+        background-image: linear-gradient(45deg, #059dff, #36c6f0);
+        opacity: 0.25;
+        -webkit-filter: blur(100px);
+        filter: blur(100px);
+    }
+
+    .gradient-circle.two {
+        left: auto;
+        top: -250px;
+        right: -250px;
+        bottom: auto;
+        background-image: -webkit-linear-gradient(45deg, #fb5343, #6549d5);
+        background-image: -o-linear-gradient(45deg, #fb5343, #6549d5);
+        background-image: linear-gradient(45deg, #fb5343, #6549d5);
+    }
 }
 </style>
--- src/sections/Navbar.vue
+++ src/sections/Navbar.vue
@@ -2,13 +2,31 @@
     <header>
         <nav class="max-width">
             <div>
-                <h1 class="gradient">{{data.logo}}</h1>
+                <h1 class="gradient">{{ data.logo }}</h1>
             </div>
-            <ul>
+            <ul class="desktop-only">
                 <li v-for="(m, i) in data.menu" :key="i">
-                    <a :href="m.id">{{m.name}}</a>
+                    <a :href="m.id">{{ m.name }}</a>
                 </li>
             </ul>
+            <div class="mobile-only">
+                <div class="btn" @click="toggleMenu">
+                    ☰
+                </div>
+                <div class="menu-overlay" :class="menuStatus ? 'active': ''">
+                    <menu>
+                        <div class="menu-header">
+                            <div class="menu-header-title gradient">Menu</div>
+                            <div class="menu-header-close" @click="toggleMenu">&times;</div>
+                        </div>
+                        <ul class="menu-body">
+                            <li v-for="(m, i) in data.menu" :key="i">
+                                <a :href="m.id">{{ m.name }}</a>
+                            </li>
+                        </ul>
+                    </menu>
+                </div>
+            </div>
         </nav>
     </header>
 </template>
@@ -19,8 +37,14 @@ import content from "../../public/data/content.json"
 export default {
     name: "Navbar",
     data: () => ({
-        data: content.navbar
-    })
+        data: content.navbar,
+        menuStatus: false
+    }),
+    methods: {
+        toggleMenu() {
+            this.menuStatus = !this.menuStatus
+        }
+    }
 }
 </script>
 
@@ -46,6 +70,75 @@ header {
             margin: 0;
         }
 
+        .desktop-only {
+            display: none;
+
+            @media screen and (min-width: $screen-sm) {
+                display: flex;
+            }
+        }
+
+        .mobile-only {
+            align-self: center;
+
+            @media screen and (min-width: $screen-sm) {
+                display: none;
+            }
+
+            .btn {
+                font-size: 2.5rem;
+                line-height: 1;
+                padding: 4px;
+                cursor: pointer;
+            }
+
+            .menu-overlay {
+                height: 100%;
+                width: 0;
+                margin: 0;
+                position: fixed;
+                z-index: 10;
+                right: 0;
+                top: 0;
+                background-color: rgba(0, 0, 0, 0.95);
+                overflow: hidden;
+                transition: 0.5s;
+
+                &.active {
+                    width: 100%;
+                }
+
+                menu {
+                    margin: 0;
+                    padding: 0;
+
+                    .menu-header {
+                        display: flex;
+                        flex-direction: row;
+                        justify-content: space-between;
+                        padding: 8px 16px;
+
+                        .menu-header-title, .menu-header-close {
+                            font-size: 3rem;
+                        }
+
+                        .menu-header-close {
+                            cursor: pointer;
+                            padding: 4px;
+                        }
+                    }
+
+                    ul {
+                        flex-direction: column;
+
+                        li {
+                            font-size: 1.5rem;
+                        }
+                    }
+                }
+            }
+        }
+
         ul {
             display: flex;
             list-style: none;
@@ -59,6 +152,13 @@ header {
                 a {
                     text-decoration: none;
                     color: $primary;
+
+                    &:hover {
+                        background: $gradient;
+                        -webkit-background-clip: text;
+                        -webkit-text-fill-color: transparent;
+                        width: max-content;
+                    }
                 }
             }
         }
