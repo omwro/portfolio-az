6456d8f3eb396adf20f0fcf4c89b0480d5fc3d1b
Omer Erdem <m.o.erdem@outlook.com>
2022-02-28 22:22:23 +0100

    about hobbies added

--- src/main.js
+++ src/main.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faLightbulb, faRobot, faCircleQuestion, faEye, faEnvelope, faAnglesUp } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+    faLightbulb,
+    faRobot,
+    faCircleQuestion,
+    faEye,
+    faEnvelope,
+    faAnglesUp,
+    faGamepad,
+    faMusic,
+    faDumbbell,
+    faSeedling,
+    faPlane,
+    faGraduationCap
+} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
 
-library.add(faLightbulb, faRobot, faCircleQuestion, faEye, faEnvelope, faLinkedin, faAnglesUp)
+library.add(
+    faLightbulb,
+    faRobot,
+    faCircleQuestion,
+    faEye,
+    faEnvelope,
+    faLinkedin,
+    faAnglesUp,
+    faGamepad,
+    faMusic,
+    faDumbbell,
+    faSeedling,
+    faPlane,
+    faGraduationCap
+)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
--- src/sections/About.vue
+++ src/sections/About.vue
@@ -1,6 +1,17 @@
 <template>
     <section id="about" class="max-width">
-        <h1 class="gradient">{{data.title}}</h1>
+        <h1 class="gradient">{{ data.title }}</h1>
+        <h2 class="gradient">Hobbies</h2>
+        <div class="container">
+            <div class="hobby-block" v-for="(h, i) in data.hobbies" :key="i">
+                <div class="hobby-icon-border">
+                    <div class="hobby-icon">
+                        <font-awesome-icon class="icon" :icon="h.icon"/>
+                    </div>
+                </div>
+                <div class="hobby-text">{{ h.name }}</div>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -15,6 +26,55 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "src/main";
 
+h1 {
+    margin-bottom: 32px;
+}
+
+h2 {
+    margin: 0 auto 32px;
+    font-size: 2rem;
+    text-align: center;
+}
+
+.container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    .hobby-block {
+        display: flex;
+        flex-direction: column;
+        margin: 8px;
+        text-align: center;
+
+        .hobby-icon-border {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 64px;
+            height: 64px;
+            background: $gradient;
+            border-radius: 40% 20%;
+
+            .hobby-icon {
+                display: flex;
+                width: 56px;
+                height: 56px;
+                font-size: 3rem;
+                justify-content: center;
+                align-items: center;
+                background: $background;
+                border-radius: 40% 20%;
+            }
+        }
+
+        .hobby-text {
+            margin-top: 4px;
+        }
+    }
+}
 </style>
